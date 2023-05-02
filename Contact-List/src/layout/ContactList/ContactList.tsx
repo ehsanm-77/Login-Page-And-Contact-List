1803eef00d34c650bd54c40627c37d37a7f68e26
ehsanm <ehsanmohamadi780@gmail.com>
2023-05-02 05:33:51 +0430

    add new styles

--- Contact-List/src/layout/ContactList/ContactList.tsx
+++ Contact-List/src/layout/ContactList/ContactList.tsx
@@ -170,29 +170,36 @@ const ContactList: React.FC = () => {
     <div
       className={`mx-auto px-4 ${mode === 'dark' ? 'dark-mode' : 'light-mode'}`}
     >
-      <div className="text-3xl font-bold text-center mb-3 text-blue-700 bg-sky-200 relative rounded-b-full top-0 p-2">
-        وب اپلیکیشن مدیریت مخاطبین
+      <div className="text-xl md:text-3xl font-bold text-center mb-3 text-white bg-[url(./src/assets/img/header.jpg)] bg-cover bg-center relative rounded-b-full top-0 p-2 drop-shadow-md">
+        <p className="drop-shadow-md">وب اپلیکیشن مدیریت مخاطبین</p>
+        <button
+          className="absolute left-10 text-sm top-2 bg-white rounded-full p-1"
+          onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}
+        >
+          {mode === 'dark' ? (
+            <img src="./src/assets/img/moon.svg" alt="aa" />
+          ) : (
+            <img src="./src/assets/img/sun.svg" alt="aa" />
+          )}
+        </button>
       </div>
-      <button onClick={() => setMode(mode === 'dark' ? 'light' : 'dark')}>
-        {mode === 'dark' ? 'Light Mode' : 'Dark Mode'}
-      </button>
       <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
         <div className="mb-4">
-          <h2 className="text-2xl text-center font-bold mb-2">
+          <h2 className="text-2xl text-center font-bold mb-2 drop-shadow-md">
             اضافه / ویرایش کاربران
           </h2>
           <form
             onSubmit={handleSubmit}
-            className={`bg-white shadow-md rounded px-8 ${
+            className={`bg-white shadow-md drop-shadow-md rounded px-8 pt-3 ${
               mode == 'dark'
-                ? 'bg-gray-700 text-white'
+                ? 'bg-gray-800 text-white'
                 : 'bg-gray-100 text-gray-700'
             }`}
           >
-            <div className="mb-4">
+            <div className="mb-3">
               <label
                 htmlFor="firstName"
-                className="block text-sm font-bold mb-2"
+                className="block text-sm font-bold mb-2 drop-shadow-md"
               >
                 نام:
               </label>
@@ -202,17 +209,17 @@ const ContactList: React.FC = () => {
                 type="text"
                 value={formData.firstName}
                 onChange={handleChange}
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline drop-shadow-md"
                 placeholder="نام ... "
               />
               <span className="text-red-500 text-sm">
                 {formErrors.firstName}
               </span>
             </div>
-            <div className="mb-4">
+            <div className="mb-3">
               <label
                 htmlFor="lastName"
-                className="block text-sm font-bold mb-2"
+                className="block text-sm font-bold mb-2 drop-shadow-md"
               >
                 نام خانوادگی:
               </label>
@@ -222,17 +229,17 @@ const ContactList: React.FC = () => {
                 type="text"
                 value={formData.lastName}
                 onChange={handleChange}
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline drop-shadow-md"
                 placeholder="نام خانوادگی ... "
               />
               <span className="text-red-500 text-sm">
                 {formErrors.lastName}
               </span>
             </div>
-            <div className="mb-4">
+            <div className="mb-3">
               <label
                 htmlFor="phoneNumber"
-                className="block text-sm font-bold mb-2"
+                className="block text-sm font-bold mb-2 drop-shadow-md"
               >
                 شماره موبایل:
               </label>
@@ -242,17 +249,17 @@ const ContactList: React.FC = () => {
                 type="text"
                 value={formData.phoneNumber}
                 onChange={handleChange}
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline drop-shadow-md"
                 placeholder="شماره موبایل  ... "
               />
               <span className="text-red-500 text-sm">
                 {formErrors.phoneNumber}
               </span>
             </div>
-            <div className="mb-4">
+            <div className="mb-3">
               <label
                 htmlFor="relation"
-                className="block text-sm font-bold mb-2"
+                className="block text-sm font-bold mb-2 drop-shadow-md"
               >
                 نسبت:
               </label>
@@ -261,7 +268,7 @@ const ContactList: React.FC = () => {
                 name="relation"
                 value={formData.relation}
                 onChange={handleChange}
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline drop-shadow-md"
               >
                 <option value="">نسبت</option>
                 <option value="اعضای خانواده">اعضای خانواده</option>
@@ -273,8 +280,11 @@ const ContactList: React.FC = () => {
                 {formErrors.relation}
               </span>
             </div>
-            <div className="mb-4">
-              <label htmlFor="email" className="block text-sm font-bold mb-2">
+            <div className="mb-3">
+              <label
+                htmlFor="email"
+                className="block text-sm font-bold mb-2 drop-shadow-md"
+              >
                 ایمیل:
               </label>
               <input
@@ -283,23 +293,25 @@ const ContactList: React.FC = () => {
                 type="text"
                 value={formData.email}
                 onChange={handleChange}
-                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                className="appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline drop-shadow-md"
                 placeholder="ایمیل ... "
               />
               <span className="text-red-500 text-sm">{formErrors.email}</span>
             </div>
             <button
               type="submit"
-              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-3"
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mb-3 shadow-md drop-shadow-md"
             >
               {editingContactId ? 'ویرایش' : 'اضافه کردن'}
             </button>
           </form>
         </div>
         <div>
-          <h2 className="text-2xl text-center font-bold mb-2">لیست کاربران</h2>
+          <h2 className="text-2xl text-center font-bold mb-2 drop-shadow-md">
+            لیست کاربران
+          </h2>
           <div
-            className={`grid grid-cols-1 md:grid-cols-2 gap-4 shadow-md rounded-md h-[465px] overflow-y-scroll ${
+            className={`grid grid-cols-1 md:grid-cols-2 gap-4 shadow-md drop-shadow-md rounded-md h-[454px] overflow-y-scroll ${
               mode == 'dark'
                 ? 'bg-gray-700 text-white'
                 : 'bg-gray-100 text-gray-700'
@@ -312,19 +324,19 @@ const ContactList: React.FC = () => {
                   mode == 'dark'
                     ? 'bg-gray-600 text-white'
                     : 'bg-gray-200 text-gray-700'
-                } shadow rounded px-8 pt-6 pb-4 flex flex-col justify-between h-[200px] m-2 shadow-md`}
+                } rounded px-8 pt-6 pb-4 flex flex-col justify-between h-[200px] m-2 shadow-md drop-shadow-md`}
               >
                 <div>
-                  <p>
+                  <p className="drop-shadow-md">
                     <strong>نام:</strong> {contact.firstName} {contact.lastName}
                   </p>
-                  <p>
+                  <p className="drop-shadow-md">
                     <strong>شماره موبایل:</strong> {contact.phoneNumber}
                   </p>
-                  <p>
+                  <p className="drop-shadow-md">
                     <strong>نسبت:</strong> {contact.relation}
                   </p>
-                  <p>
+                  <p className="drop-shadow-md">
                     <strong>ایمیل:</strong> {contact.email}
                   </p>
                 </div>
@@ -332,13 +344,13 @@ const ContactList: React.FC = () => {
                 <div className="flex justify-end mt-4">
                   <button
                     onClick={() => handleEdit(contact.id)}
-                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline mr-2"
+                    className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-r-md focus:outline-none focus:shadow-outline mr-2 shadow-md drop-shadow-md"
                   >
                     ویرایش
                   </button>
                   <button
                     onClick={() => handleDelete(contact)}
-                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                    className="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded-l-md focus:outline-none focus:shadow-outline shadow-md drop-shadow-md"
                   >
                     حذف
                   </button>
